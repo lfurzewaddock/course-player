6ff25d191b0fb9a2fe10b50e968a010fd272a030
Leo Furze-Waddock <leo@furze-waddock.com>
2017-08-26 00:15:24 +0100

    1st test passing!

--- source/test/index.js
+++ source/test/index.js
@@ -1,2 +1,27 @@
+import React from 'react';
 import test from 'tape';
-import card from 'components/card';
+import { shallow } from 'enzyme';
+
+import cardPlayer from 'components/card-player';
+
+test('CardPlayer', assert => {
+  const msg = 'CardPlayer should render something.';
+
+  // Inject React into the factory to enable loading React from a CDN,
+  // which may be cached, to boost performance and support multiple versions.
+  const CardPlayer = cardPlayer(React);
+
+  const courseTitle = 'My Course';
+  const props = {
+    courseTitle
+  };
+
+  const $ = shallow(<CardPlayer { ...props } />);
+  const output = $.find('.course-title').text();
+
+  const actual = output;
+  const expected = courseTitle;
+
+  assert.equal(actual, expected, msg);
+  assert.end();
+});
