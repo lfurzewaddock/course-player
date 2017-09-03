cc769e3c0cd5c0a155ae15c665a08a9ffb9e3639
Leo Furze-Waddock <leo@furze-waddock.com>
2017-09-03 19:02:35 +0100

    11 tests passing!

--- source/components/card-player.js
+++ source/components/card-player.js
@@ -1,17 +1,34 @@
-export default React => ({ courseTitle, cardContent, isCompleted }) => (
-  <div className="card-player">
-    <div className="nav-bar">
-      <h1 className="course-title">{ courseTitle }</h1>
-      <nav className="nav">
-        <select>
-          <option selected="selected">Card 1</option>
-          <option>Card 2</option>
-          <option>Card 3</option>
-        </select>
-      </nav>
+const makeClasses = (...classes) => {
+  return classes.join(' ').trim();
+};
+
+export default React => {
+
+  const CardListItem = ({card, currentCardId}) => (
+    <li key={card.id}
+     className={ makeClasses('card-list-item',
+      card.id === currentCardId ? 'current-card' : '')
+    }>{ card.name }</li>
+  );
+
+  return ({ courseTitle, cardContent, isCompleted, cardsList, currentCardId }) => (
+    <div className="card-player">
+      <div className="nav-bar">
+        <h1 className="course-title">{ courseTitle }</h1>
+      </div>
+      <div className="card-container">
+        <div className="card-content">{ cardContent }</div>
+        <div className="next-card"><div className="spacer"></div>
+          <button className="button" disabled={ !isCompleted }>Continue</button>
+        </div>
+      </div>
+      <ul className="card-list">
+        {
+          cardsList.map(card => {
+            return CardListItem({ card, currentCardId } );
+          })
+        }
+      </ul>
     </div>
-    <div className="card-content">{ cardContent }</div>
-    <div className="next-card"><div className="spacer"></div>
-    <button className="button" disabled={ !isCompleted }>Continue</button></div>
-  </div>
-);
+  );
+};
--- source/test/index.js
+++ source/test/index.js
@@ -4,6 +4,28 @@ import { shallow } from 'enzyme';
 
 import cardPlayer from 'components/card-player';
 
+// Factory function to return props
+const makeProps = (settings) => Object.assign({}, {
+  courseTitle: 'My Course',
+  cardContent: 'Contents...',
+  isCompleted: false,
+  cardsList: [
+    {
+      id: '1',
+      name: 'Card 1'
+    },
+    {
+      id: '2',
+      name: 'Card 2'
+    },
+    {
+      id: '3',
+      name: 'Card 3'
+    }
+  ],
+  currentCardId: '1'
+}, settings);
+
 test('CardPlayer', nest => {
   nest.test('...card-player', assert => {
     const msg = 'CardPlayer should render the card-player div.';
@@ -12,10 +34,7 @@ test('CardPlayer', nest => {
     // which may be cached, to boost performance and support multiple versions.
     const CardPlayer = cardPlayer(React);
 
-    const courseTitle = 'My Course';
-    const props = {
-      courseTitle
-    };
+    const props = makeProps();
 
     const $ = shallow(<CardPlayer { ...props } />);
     const output = $.find('.card-player').children().length;
@@ -33,10 +52,7 @@ test('CardPlayer', nest => {
     // which may be cached, to boost performance and support multiple versions.
     const CardPlayer = cardPlayer(React);
 
-    const courseTitle = 'My Course';
-    const props = {
-      courseTitle
-    };
+    const props = makeProps();
 
     const $ = shallow(<CardPlayer { ...props } />);
     const output = $.find('.nav-bar').children().length;
@@ -54,37 +70,16 @@ test('CardPlayer', nest => {
     // which may be cached, to boost performance and support multiple versions.
     const CardPlayer = cardPlayer(React);
 
-    const courseTitle = 'My Course';
-    const props = {
+    const courseTitle = 'My Amazing Course';
+    const props = makeProps( {
       courseTitle
-    };
+    });
 
     const $ = shallow(<CardPlayer { ...props } />);
     const output = $.find('.course-title').text();
 
     const actual = output;
     const expected = courseTitle;
-
-    assert.equal(actual, expected, msg);
-    assert.end();
-  });
-  nest.test('...nav', assert => {
-    const msg = 'CardPlayer should render the nav.';
-
-    // Inject React into the factory to enable loading React from a CDN,
-    // which may be cached, to boost performance and support multiple versions.
-    const CardPlayer = cardPlayer(React);
-
-    const courseTitle = 'My Course';
-    const props = {
-      courseTitle
-    };
-
-    const $ = shallow(<CardPlayer { ...props } />);
-    const output = $.find('.nav').children().length;
-
-    const actual = output > 0;
-    const expected = true;
 
     assert.equal(actual, expected, msg);
     assert.end();
@@ -96,12 +91,10 @@ test('CardPlayer', nest => {
     // which may be cached, to boost performance and support multiple versions.
     const CardPlayer = cardPlayer(React);
 
-    const courseTitle = 'My Course';
     const cardContent = 'My content';
-    const props = {
-      courseTitle,
+    const props = makeProps({
       cardContent
-    };
+    });
 
     const $ = shallow(<CardPlayer { ...props } />);
     const output = $.find('.card-content').text();
@@ -119,10 +112,7 @@ test('CardPlayer', nest => {
     // which may be cached, to boost performance and support multiple versions.
     const CardPlayer = cardPlayer(React);
 
-    const courseTitle = 'My Course';
-    const props = {
-      courseTitle
-    };
+    const props = makeProps();
 
     const $ = shallow(<CardPlayer { ...props } />);
     const output = $.find('.next-card').children().length;
@@ -140,11 +130,7 @@ test('CardPlayer', nest => {
     // which may be cached, to boost performance and support multiple versions.
     const CardPlayer = cardPlayer(React);
 
-    const courseTitle = 'My Course';
-    const props = {
-      courseTitle,
-      isCompleted: false
-    };
+    const props = makeProps({ isCompleted: false });
 
     const $ = shallow(<CardPlayer { ...props } />);
     const output = $.find('.button').html().includes('disabled');
@@ -162,17 +148,99 @@ test('CardPlayer', nest => {
     // which may be cached, to boost performance and support multiple versions.
     const CardPlayer = cardPlayer(React);
 
-    const courseTitle = 'My Course';
-    const props = {
-      courseTitle,
-      isCompleted: true
-    };
+    const props = makeProps({ isCompleted: true });
 
     const $ = shallow(<CardPlayer { ...props } />);
     const output = $.find('.button').html().includes('disabled');
 
     const actual = output;
     const expected = false;
+
+    assert.equal(actual, expected, msg);
+    assert.end();
+  });
+  nest.test('...card-container', assert => {
+    const msg = 'CardPlayer should render the card-container.';
+
+    // Inject React into the factory to enable loading React from a CDN,
+    // which may be cached, to boost performance and support multiple versions.
+    const CardPlayer = cardPlayer(React);
+
+    const props = makeProps();
+
+    const $ = shallow(<CardPlayer { ...props } />);
+    const output = $.find('.card-container').children().length;
+
+    const actual = output > 0;
+    const expected = true;
+
+    assert.equal(actual, expected, msg);
+    assert.end();
+  });
+  nest.test('...card-list', assert => {
+    const msg = 'CardPlayer should render the card-list.';
+
+    // Inject React into the factory to enable loading React from a CDN,
+    // which may be cached, to boost performance and support multiple versions.
+    const CardPlayer = cardPlayer(React);
+
+    const props = makeProps();
+
+    const $ = shallow(<CardPlayer { ...props } />);
+    const output = $.find('.card-list').children().length;
+
+    const actual = output > 0;
+    const expected = true;
+
+    assert.equal(actual, expected, msg);
+    assert.end();
+  });
+  nest.test('...card-list contents', assert => {
+    const msg = 'CardPlayer should render the correct card-list.';
+
+    // Inject React into the factory to enable loading React from a CDN,
+    // which may be cached, to boost performance and support multiple versions.
+    const CardPlayer = cardPlayer(React);
+
+    const props = makeProps({ cardsList: [
+        {
+          id: '1',
+          name: 'Card #1'
+        },
+        {
+          id: '2',
+          name: 'Card #2'
+        },
+        {
+          id: '3',
+          name: 'Card #3'
+        }
+      ]
+    });
+
+    const $ = shallow(<CardPlayer { ...props } />);
+    const output = $.find('.card-list li').length;
+
+    const actual = output;
+    const expected = 3;
+
+    assert.equal(actual, expected, msg);
+    assert.end();
+  });
+  nest.test('...card-list current card', assert => {
+    const msg = 'CardPlayer should render the current card.';
+
+    // Inject React into the factory to enable loading React from a CDN,
+    // which may be cached, to boost performance and support multiple versions.
+    const CardPlayer = cardPlayer(React);
+
+    const props = makeProps({ currentCardId: '3' });
+
+    const $ = shallow(<CardPlayer { ...props } />);
+    const output = $.find('.card-list').children().last().hasClass('current-card');
+
+    const actual = output;
+    const expected = true;
 
     assert.equal(actual, expected, msg);
     assert.end();
